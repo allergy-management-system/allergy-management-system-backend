8b7dbdbb5ec944183674be95827b8e2ee66e8b5f
Funky2003 <nusetorsetsofia101@gmail.com>
2024-03-08 16:38:42 +0000

    Docker file update

--- allergy/src/main/java/com/allergy/allergy/AllergyApplicationConfiguration.java
+++ allergy/src/main/java/com/allergy/allergy/AllergyApplicationConfiguration.java
@@ -5,6 +5,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AllergyApplicationConfiguration {
     public AllergyApplicationConfiguration() {
-        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "/allergy-management-service-843d91d86665.json");
+        System.setProperty("GOOGLE_APPLICATION_CREDENTIALS", "/allergy/allergy-management-service-843d91d86665.json");
     }
 }
