efc22b177ef435de3a5e2c37a695c5ee83e8916b
Funky2003 <nusetorsetsofia101@gmail.com>
2024-03-09 12:18:35 +0000

    Code Update

--- allergy/src/main/java/com/allergy/allergy/AllergyRecommendationSection/constants/AllergyConstants.java
+++ allergy/src/main/java/com/allergy/allergy/AllergyRecommendationSection/constants/AllergyConstants.java
@@ -4,13 +4,17 @@ public class AllergyConstants {
     private String projectId = "allergy-management-service";
     private String projectLocation = "us-central1";
     private String modelName = "gemini-pro-vision";
+    private String key = "AIzaSyBekECJcc99ByfMA_j-gjCqYW4vcoWFzuc";
+//    private String credentials = "allergy/service.json";
 
     public AllergyConstants() {}
 
-    public AllergyConstants(String projectId, String projectLocation, String modelName) {
+    public AllergyConstants(String projectId, String projectLocation, String modelName, String key, String credentials) {
         this.projectId = projectId;
         this.projectLocation = projectLocation;
         this.modelName = modelName;
+        this.key = key;
+//        this.credentials = credentials;
     }
 
     public String getProjectId() {
@@ -24,4 +28,8 @@ public class AllergyConstants {
     public String getModelName() {
         return modelName;
     }
+
+    public String getKey() { return key; }
+
+//    public String getCredentials() { return credentials; }
 }
--- allergy/src/main/java/com/allergy/allergy/AllergyRecommendationSection/service/AllergyService.java
+++ allergy/src/main/java/com/allergy/allergy/AllergyRecommendationSection/service/AllergyService.java
@@ -1,12 +1,16 @@
 package com.allergy.allergy.AllergyRecommendationSection.service;
 
 import com.allergy.allergy.AllergyRecommendationSection.constants.AllergyConstants;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.generativeai.preview.ChatSession;
 import com.google.cloud.vertexai.generativeai.preview.GenerativeModel;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 @Service
@@ -16,8 +20,15 @@ public class AllergyService {
     public String allergyAiChatResponse(String message) throws IOException {
         AllergyConstants constants = new AllergyConstants();
 
-        String apiKey = "AIzaSyBekECJcc99ByfMA_j-gjCqYW4vcoWFzuc";
-        VertexAI vertexAI = new VertexAI(constants.getProjectId(), constants.getProjectLocation(), apiKey); // Pass API key
+        //set the  google credentials
+        //String googleCredentialsString = constants.getCredentials();
+
+        //create google credentials from the credential string
+        GoogleCredentials googleCredentials = GoogleCredentials.fromStream(
+                new FileInputStream("allergy/service.json")
+        );
+
+        VertexAI vertexAI = new VertexAI(constants.getProjectId(), constants.getProjectLocation(), constants.getKey()); // Pass API key
         GenerativeModel generativeModel = new GenerativeModel(constants.getModelName(), vertexAI);
         ChatSession chatSession = new ChatSession(generativeModel);
         GenerateContentResponse contentResponse = chatSession.sendMessage(message);
