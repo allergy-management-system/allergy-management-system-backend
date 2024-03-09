8375ab4c5be21aa304af2c1d826328aa159a14f5
Funky2003 <nusetorsetsofia101@gmail.com>
2024-03-09 09:05:57 +0000

    Updates

--- allergy/src/main/java/com/allergy/allergy/AllergyApplication.java
+++ allergy/src/main/java/com/allergy/allergy/AllergyApplication.java
@@ -14,26 +14,11 @@ import static com.google.auth.oauth2.GoogleCredentials.*;
 
 @SpringBootApplication
 public class AllergyApplication implements CommandLineRunner {
-	static final String SERVICE_ACCOUNT_KEY_FILE = "service.json";
 	public static void main(String[] args) {
 		SpringApplication.run(AllergyApplication.class, args);
-		try {
-			Credentials credentials = loadCredentials();
-			// Use the credentials to authenticate with Google service(s)
-			// ...
-		} catch (IOException e) {
-			System.err.println("Error loading credentials: " + e.getMessage());
-			// Handle the exception appropriately
-		}
 
 	}
 
 	@Override
 	public void run(String... args) {}
-
-	private static Credentials loadCredentials() throws IOException {
-		FileInputStream serviceAccountStream = new FileInputStream("allergy/service.json");
-		return GoogleCredentials.fromStream(serviceAccountStream)
-				.createScoped(/* list of desired scopes */);
-	}
 }
