ab62a024ba57bd0824dff416a1549c4c632b694b
Funky2003 <nusetorsetsofia101@gmail.com>
2024-03-09 10:24:06 +0000

    Added gcloud api-key

--- allergy/src/main/java/com/allergy/allergy/AllergyRecommendationSection/service/AllergyService.java
+++ allergy/src/main/java/com/allergy/allergy/AllergyRecommendationSection/service/AllergyService.java
@@ -5,16 +5,19 @@ import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.generativeai.preview.ChatSession;
 import com.google.cloud.vertexai.generativeai.preview.GenerativeModel;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 
 @Service
 public class AllergyService {
+
+
     public String allergyAiChatResponse(String message) throws IOException {
         AllergyConstants constants = new AllergyConstants();
 
-        VertexAI vertexAI = new VertexAI(constants.getProjectId(), constants.getProjectLocation());
+        String apiKey = "AIzaSyBekECJcc99ByfMA_j-gjCqYW4vcoWFzuc";
+        VertexAI vertexAI = new VertexAI(constants.getProjectId(), constants.getProjectLocation(), apiKey); // Pass API key
         GenerativeModel generativeModel = new GenerativeModel(constants.getModelName(), vertexAI);
         ChatSession chatSession = new ChatSession(generativeModel);
         GenerateContentResponse contentResponse = chatSession.sendMessage(message);
